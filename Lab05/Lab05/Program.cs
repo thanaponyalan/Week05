55857e9da20d42e79b7cb5dc2c8b86982a318391
thanaponyalan <thanaponyalan@gmail.com>
2018-09-14 15:09:24 +0700

    Programs with Try Catch

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -7,7 +7,15 @@ namespace Lab05
         static void Main(string[] args)
         {
             object o2 = null;
-            int i2 = (int)o2;   // Error
+            try
+            {
+                int i2 = (int)o2;
+                Console.WriteLine("i2 = {0}", i2);
+            }
+            catch
+            {
+                Console.WriteLine("Error, null object assignment.");
+            }
         }
     }
 }
