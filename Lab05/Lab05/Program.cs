67dfe4119132779d99ac73fc1f569cac10ed8ff8
thanaponyalan <thanaponyalan@gmail.com>
2018-09-14 14:58:39 +0700

    Non use try catch finally

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,46 +6,8 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            int x = 200, y = 4;
-            int count = 0;
-            string[,] array = new string[x, y];
-
-            // Initialize the array:
-            for (int i = 0; i < x; i++)
-                for (int j = 0; j < y; j++)
-                    array[i, j] = (++count).ToString();
-
-            // Read input:
-            Console.Write("Enter the number to search for: ");
-
-            // Input a string:
-            string myNumber = Console.ReadLine();
-
-            // Search:
-            for (int i = 0; i < x; i++)
-            {
-                for (int j = 0; j < y; j++)
-                {
-                    if (array[i, j].Equals(myNumber))
-                    {
-                        goto Found;
-                    }
-                }
-            }
-
-            Console.WriteLine("The number {0} was not found.", myNumber);
-            goto Finish;
-
-        Found:
-            Console.WriteLine("The number {0} is found.", myNumber);
-
-        Finish:
-            Console.WriteLine("End of search.");
-
-            // Keep the console open in debug mode.
-            Console.WriteLine("Press any key to exit.\n");
-            Console.ReadKey();
-            Main(args);
+            object o2 = null;
+            int i2 = (int)o2;   // Error
         }
     }
 }
