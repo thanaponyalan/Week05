e907bc9c30e28385b27a67cda466c8d42900be2b
thanaponyalan <thanaponyalan@gmail.com>
2018-09-14 15:56:38 +0700

    nothing

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,20 +6,7 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            int a = 0;
-            try
-            {
-
-                Console.WriteLine(100 / a);
-            }
-            catch (NullReferenceException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            
         }
     }
 }
