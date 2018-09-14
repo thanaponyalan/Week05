587d51f9dbacf8b8d456339487bd8045458d4d4a
thanaponyalan <thanaponyalan@gmail.com>
2018-09-14 14:03:41 +0700

    2nd Commit

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab05
 {
@@ -10,6 +6,7 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
