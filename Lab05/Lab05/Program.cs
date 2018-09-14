526697b9272bb57c330c116a5f7fe9f8387ea4a4
thanaponyalan <thanaponyalan@gmail.com>
2018-09-14 14:09:32 +0700

    goto n' excercise

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,7 +6,24 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Line 1");
+            goto line4;
+        line2:
+            Console.WriteLine("Line 2");
+            goto line9;
+            Console.WriteLine("Line 3");
+        line4:
+            Console.WriteLine("Line 4");
+            Console.WriteLine("Line 5");
+            goto line2;
+            Console.WriteLine("Line 6");
+            goto line10;
+            Console.WriteLine("Line 7");
+            Console.WriteLine("Line 8");
+        line9:
+            Console.WriteLine("Line 9");
+        line10:;
+            //Console.WriteLine("Line 10");
         }
     }
 }
