63b022777e0b54d2a9debe7b924ff84e8dca7b23
thanaponyalan <thanaponyalan@gmail.com>
2018-09-14 14:35:02 +0700

    goto with loop

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,31 +6,41 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coffee sizes: 1=Small 2=Medium 3=Large");
-            Console.Write("Please enter your selection: ");
-            string s = Console.ReadLine();
-            int n = int.Parse(s);
-            int cost = 0;
-            switch (n)
+            int x = 200, y = 4;
+            int count = 0;
+            string[,] array = new string[x, y];
+
+            // Initialize the array:
+            for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                    array[i, j] = (++count).ToString();
+
+            // Read input:
+            Console.Write("Enter the number to search for: ");
+
+            // Input a string:
+            string myNumber = Console.ReadLine();
+
+            // Search:
+            for (int i = 0; i < x; i++)
             {
-                case 1:
-                    cost += 25;
-                    break;
-                case 2:
-                    cost += 25;
-                    goto case 1;
-                case 3:
-                    cost += 50;
-                    goto case 1;
-                default:
-                    Console.WriteLine("Invalid selection.");
-                    break;
+                for (int j = 0; j < y; j++)
+                {
+                    if (array[i, j].Equals(myNumber))
+                    {
+                        goto Found;
+                    }
+                }
             }
-            if (cost != 0)
-            {
-                Console.WriteLine("Please insert {0} Bath.", cost);
-            }
-            Console.WriteLine("Thank you for your business.");
+
+            Console.WriteLine("The number {0} was not found.", myNumber);
+            goto Finish;
+
+        Found:
+            Console.WriteLine("The number {0} is found.", myNumber);
+
+        Finish:
+            Console.WriteLine("End of search.");
 
             // Keep the console open in debug mode.
             Console.WriteLine("Press any key to exit.\n");
