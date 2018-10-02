e5187c1290d1c3fff6597b60859a9c731f9f2e24
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 21:25:09 +0700

    while

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,37 +6,12 @@ namespace Lab05
     {
         public static void Main()
         {
-            Console.Write("Input first three char of day(sun, mon, tue, wed, thu, fri or sat) : ");
-            string gradeString = Console.ReadLine();
-            string message;
-            switch (gradeString.ToLower())
+            int n = 1;
+            while (n < 6)
             {
-                case "sun":
-                    message = "Sunday, color Red";
-                    break;
-                case "mon":
-                    message = "Monday, color Yellow";
-                    break;
-                case "tue":
-                    message = "Tuesday, color Pink";
-                    break;
-                case "wed":
-                    message = "Wednesday, color Green";
-                    break;
-                case "thu":
-                    message = "Thursday, color Orange";
-                    break;
-                case "fri":
-                    message = "Friday, color Blue";
-                    break;
-                case "sat":
-                    message = "Saturday, color Purple";
-                    break;
-                default:
-                    message = "Incorrect day";
-                    break;
+                Console.WriteLine("Current value of n is {0}", n);
+                n++;
             }
-            Console.WriteLine(message);
 
         }
     }
