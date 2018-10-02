3c16ba64e958171d55687e123e7b07e576528b2c
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 21:36:34 +0700

    break 2

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,18 +6,30 @@ namespace Lab05
     {
         public static void Main()
         {
-            for (int i = 1; i <= 100; i++)
+            Console.Write("Enter your selection (1, 2, or 3): ");
+            string s = Console.ReadLine();
+            int n = Int32.Parse(s);
+
+            switch (n)
             {
-                if (i == 5)
-                {
+                case 1:
+                    Console.WriteLine("Current value is {0}", 1);
                     break;
-                }
-                Console.WriteLine(i);
+                case 2:
+                    Console.WriteLine("Current value is {0}", 2);
+                    break;
+                case 3:
+                    Console.WriteLine("Current value is {0}", 3);
+                    break;
+                default:
+                    Console.WriteLine("Sorry, invalid selection.");
+                    break;
             }
 
             // Keep the console open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+
 
         }
     }
