64479070b0074f9d3724d2e0ddde5b064fe03c25
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 21:01:03 +0700

    if 4

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,19 +6,18 @@ namespace Lab05
     {
         public static void Main()
         {
-            int a = 10;
-            int b = 20;
-            if (a == 10)
-            {
-                if (b == 20)
-                {
-                    Console.WriteLine("a = 10 and b = 20");
-                }
-                if (b != 20)
-                {
-                    Console.WriteLine("a = 10 and b != 20");
-                }
-            }
+            int point = 68; // ทดลองเปลี่ยนเป็น Console.ReadLine() เพื่อรับค่าจากผู้ใช้
+            if (point < 50)
+                Console.WriteLine("Grade F");
+            else if (point < 60)
+                Console.WriteLine("Grade D");
+            else if (point < 70)
+                Console.WriteLine("Grade C");
+            else if (point < 80)
+                Console.WriteLine("Grade B");
+            else
+                Console.WriteLine("Grade A");
+
 
         }
     }
