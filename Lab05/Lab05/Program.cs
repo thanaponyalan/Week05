e2a3dc92bb4957550ed284276b0e4abc340e32c3
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 21:27:36 +0700

    while do while

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,10 +6,24 @@ namespace Lab05
     {
         public static void Main()
         {
-            int a = 0;
-            do a = 2;
-            while (false);
-            Console.WriteLine(a);
+            Console.WriteLine("---- Begin of While statement.");
+            int n = 1;
+            while (n < 1)
+            {
+                Console.WriteLine("  Current value of n is {0}", n);
+                n++;
+            }
+            Console.WriteLine("---- End of While statement.");
+
+            Console.WriteLine("---- Begin of Do..While statement.");
+            n = 1;
+            do
+            {
+                Console.WriteLine("  Current value of n is {0}", n);
+                n++;
+            }
+            while (n < 1);
+            Console.WriteLine("---- End of Do..While statement.");
 
         }
     }
