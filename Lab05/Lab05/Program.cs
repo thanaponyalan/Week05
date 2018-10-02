50755df469c476f90f2b865a27ad3b0612c7caf1
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 21:26:33 +0700

    do while

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,12 +6,10 @@ namespace Lab05
     {
         public static void Main()
         {
-            int n = 1;
-            while (n < 6)
-            {
-                Console.WriteLine("Current value of n is {0}", n);
-                n++;
-            }
+            int a = 0;
+            do a = 2;
+            while (false);
+            Console.WriteLine(a);
 
         }
     }
