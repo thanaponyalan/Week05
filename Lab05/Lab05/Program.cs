e615354e601fd1c07e172ce85496443a74c1b9bb
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 20:52:01 +0700

    throw 2.1

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -8,12 +8,32 @@ namespace Lab05
         {
             int a = 10;
             int b = 20;
-            int c = add(a, b);
-        }
-        private static int add(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
+            int c;
+            try
+            {
+                c = div(a, b);
+            }
+            catch (DivideByZeroException e)
+            {
 
+                Console.WriteLine("DivideByZeroException");
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("NullReferenceException");
+                Console.WriteLine(e.Message);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception");
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static int div(int a, int b)
+        {
+            throw new   DivideByZeroException();
+        }
     }
 }
