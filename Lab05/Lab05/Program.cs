2fd8230398d768d4f32734487ed4dbf90912e514
thanaponyalan <thanaponyalan@gmail.com>
2018-10-02 21:03:33 +0700

    switch 1

--- Lab05/Lab05/Program.cs
+++ Lab05/Lab05/Program.cs
@@ -6,18 +6,31 @@ namespace Lab05
     {
         public static void Main()
         {
-            int point = 68; // ทดลองเปลี่ยนเป็น Console.ReadLine() เพื่อรับค่าจากผู้ใช้
-            if (point < 50)
-                Console.WriteLine("Grade F");
-            else if (point < 60)
-                Console.WriteLine("Grade D");
-            else if (point < 70)
-                Console.WriteLine("Grade C");
-            else if (point < 80)
-                Console.WriteLine("Grade B");
-            else
-                Console.WriteLine("Grade A");
-
+            Console.Write("Input your grade (A, B, C, D or F) : ");
+            string gradeString = Console.ReadLine();
+            string message;
+            switch (gradeString.ToUpper())
+            {
+                case "A":
+                    message = "Excellent";
+                    break;
+                case "B":
+                    message = "Good";
+                    break;
+                case "C":
+                    message = "Cool";
+                    break;
+                case "D":
+                    message = "Try";
+                    break;
+                case "F":
+                    message = "Get out!!";
+                    break;
+                default:
+                    message = "Incorrect grade";
+                    break;
+            }
+            Console.WriteLine(message);
 
         }
     }
